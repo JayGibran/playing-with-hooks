646c3e493dcec8ffaf9ba727bca5aa7cee015c49
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-11-03 19:47:56 +0000

    saving ingredients to firebase and fetching them with useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch("https://react-hooks-3eeb2.firebaseio.com/ingredients.json")
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setIngredients(loadedIngredients);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch("https://react-hooks-3eeb2.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        setIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
