b8fa39ce2545e70a7b9d98d3afb8ef30c83aa32d
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-11-04 15:33:57 +0000

    adding userCallback to handle props method on component to no render it multiple times

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,21 +7,8 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch("https://react-hooks-3eeb2.firebaseio.com/ingredients.json")
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setIngredients(loadedIngredients);
-      })
-      .catch(err => console.log(err));
+  const filteredIngredients = useCallback(filteredIngredients => {
+    setIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -45,7 +32,7 @@ function Ingredients() {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredients} />
         <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
